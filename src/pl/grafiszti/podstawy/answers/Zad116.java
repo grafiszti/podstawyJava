f6b26e3ac18686ecd57559fc6dad7585c603793f
matt <grafiszti@gmail.com>
2014-07-21 19:09:42 +0200

    dodana tresc do zadania

--- src/pl/grafiszti/podstawy/answers/Zad116.java
+++ src/pl/grafiszti/podstawy/answers/Zad116.java
@@ -2,6 +2,14 @@ package pl.grafiszti.podstawy.answers;
 
 import pl.grafiszti.podstawy.utils.BufferedBinaryReaderWriter;
 
+/*1.16 Korzystając z własnego buforowania przekopiuj
+ *  dane znajdujące się w pliku binarnie.txt do 
+ *  przekopiowane.txt. Dodatkowo zlicz ilość skopiowanych bajtów.
+
+ (Podpowiedź: użyj metody write(byte[] b, int off, int len), 
+ dostępnej w każdej z dzisiaj omówionych klas).
+ */
+
 public class Zad116 {
 	public void run() {
 		BufferedBinaryReaderWriter copier = new BufferedBinaryReaderWriter(
@@ -10,7 +18,7 @@ public class Zad116 {
 
 		System.out.println("Przekopiowano : " + copier.copyInputToOutput()
 				+ " bajtow");
-		
+
 		copier.close();
 	}
 }
